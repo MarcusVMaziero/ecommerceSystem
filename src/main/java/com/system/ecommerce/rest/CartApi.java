a6a7bef4dbfb4bad3233bb8b8fb5821f53083cf1
Marcus Maziero <marcus.maziero@outlook.com>
2021-11-06 16:33:46 -0300

    Cria nova endpoint para checkout

--- src/main/java/com/system/ecommerce/rest/CartApi.java
+++ src/main/java/com/system/ecommerce/rest/CartApi.java
@@ -1,14 +1,14 @@
 package com.system.ecommerce.rest;
 
 import com.system.ecommerce.core.cart.Cart;
+import com.system.ecommerce.core.cart.CartCheckout;
 import com.system.ecommerce.core.cart.CartService;
 import com.system.ecommerce.rest.request.CartRequest;
+import com.system.ecommerce.rest.request.CheckoutRequest;
 import com.system.ecommerce.rest.response.CartResponse;
+import com.system.ecommerce.rest.response.CheckoutResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cart/v1")
@@ -18,9 +18,13 @@ public class CartApi {
     private final CartService cartService;
 
     @PostMapping
-    public CartResponse postCart(@RequestBody CartRequest cartRequest) {
+    public CartResponse addProductToCart(@RequestBody CartRequest cartRequest) {
         return CartResponse.toResponse(
-                cartService.create(Cart.toService(cartRequest))
-        );
+                cartService.create(Cart.toService(cartRequest)));
+    }
+
+    @PatchMapping("checkout")
+    public CheckoutResponse getCheckout(@RequestBody CheckoutRequest checkoutRequest) {
+        return CheckoutResponse.toResponse(cartService.checkout(CartCheckout.toService(checkoutRequest)));
     }
 }
