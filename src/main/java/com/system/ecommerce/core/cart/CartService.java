81cd0899a183511929a063b6caff3ca5db2a1412
Marcus Maziero <marcus.maziero@outlook.com>
2021-11-09 20:43:25 -0300

    Altera create e cria regra do checkout

--- src/main/java/com/system/ecommerce/core/cart/CartService.java
+++ src/main/java/com/system/ecommerce/core/cart/CartService.java
@@ -1,11 +1,14 @@
 package com.system.ecommerce.core.cart;
 
+import com.system.ecommerce.core.product.Product;
 import com.system.ecommerce.core.product.ProductService;
 import com.system.ecommerce.data.cart.CartGateway;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 
 @RequiredArgsConstructor
 @Component
@@ -15,22 +18,27 @@ public class CartService {
     private final ProductService productService;
 
     public Cart create(Cart cart) {
-
-        if (cartGateway.findCartEmail(cart.getEmail()) == null) {
+        Cart cartExist = cartGateway.findCartEmail(cart.getEmail());
+        if (cartExist != null) {
+            cart.setId(cartExist.getId());
+            productService.addProductToCart(cart);
+            return cart;
+        } else {
             var cartNew = cartGateway.create(cart);
             cartNew.setAmmount(cart.getAmmount());
             cartNew.setIdProduct(cart.getIdProduct());
             productService.addProductToCart(cartNew);
             return cartNew;
-        } else {
-            productService.addProductToCart(cart);
-            return cart;
         }
     }
 
     public CartCheckout checkout(CartCheckout cartCheckout) {
-        var products = new ArrayList<>(cartGateway.findCart(cartCheckout).getProductList());
-        cartCheckout.toCartCheckout(products, 100.1);
+        var products = new ArrayList<>(cartGateway.findCartCheckout(cartCheckout).getProductList());
+        var counts = products.stream().map(Product::getValue).collect(Collectors.toList());
+        Double totalValue = counts.stream()
+                .flatMapToDouble(DoubleStream::of)
+                .sum();
+        cartCheckout.toCartCheckout(products, totalValue);
         return cartCheckout;
     }
 }
