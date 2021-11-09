953d5aea2908a51208aff01649b8cbea9d7af253
Marcus Maziero <marcus.maziero@outlook.com>
2021-11-09 20:44:58 -0300

    Cria novo metodo para buscar com filtro os produtos

--- src/main/java/com/system/ecommerce/data/product/ProductRepository.java
+++ src/main/java/com/system/ecommerce/data/product/ProductRepository.java
@@ -2,5 +2,9 @@ package com.system.ecommerce.data.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<ProductModel, Long> {
+
+    List<ProductModel> findByIdCart(Long idCard);
 }
